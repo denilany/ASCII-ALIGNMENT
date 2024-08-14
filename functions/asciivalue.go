58dc4a21ce5bc3a6b19c963e6b5817434ae267b1
danyonyi <denilanyonyi1@gmail.com>
2024-08-14 11:53:35 +0300

    fix: handle specific errors if the user enters an incorrect command line argument

--- functions/asciivalue.go
+++ functions/asciivalue.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -8,59 +9,45 @@ import (
 
 type Arguments struct {
 	input  string
-	flag   []string
+	flag   string
 	banner string
 }
 
 func ParseArguments() (Arguments, error) {
 	args := Arguments{}
-
 	cmdArgs := os.Args[1:]
 
-	if len(cmdArgs) == 1 {
-		args.input = cmdArgs[0]
-		args.flag = append(args.flag, "left")
-		args.banner = "standard"
-		return args, nil
-	} else if len(cmdArgs) == 2 {
-		if strings.Contains(cmdArgs[0], "--align=") {
-			alignment := strings.TrimPrefix(cmdArgs[0], "--align=")
-			switch alignment {
-			case "left", "right", "center", "justify":
-				args.flag = append(args.flag, alignment)
-			default:
-				return args, fmt.Errorf("invalid alignment: %s", alignment)
-			}
-			args.input = cmdArgs[1]
-
-			args.banner = "standard"
-			return args, nil
-		} else {
-			args.input = cmdArgs[0]
-			args.flag = append(args.flag, "left")
-			args.banner = cmdArgs[1]
-			return args, nil
-		}
+	if len(cmdArgs) == 0 {
+		return args, errors.New("no arguments provided")
 	}
 
-	for i, arg := range cmdArgs {
+	// Default values
+	args.flag = "left"
+	args.banner = "standard"
+
+	for _, arg := range cmdArgs {
 		if strings.HasPrefix(arg, "--align=") {
 			alignment := strings.TrimPrefix(arg, "--align=")
 			switch alignment {
 			case "left", "right", "center", "justify":
-				args.flag = append(args.flag, alignment)
+				args.flag = alignment
 			default:
 				return args, fmt.Errorf("invalid alignment: %s", alignment)
 			}
-		} else if i == 1 {
+		} else if strings.HasPrefix(arg, "--") {
+			// Catch any unrecognized flags
+			return args, errors.New("invalid option: " + arg)
+		} else if args.input == "" {
 			args.input = arg
-		} else if i == 2 {
+		} else if args.banner == "standard" {
 			args.banner = arg
+		} else {
+			return args, fmt.Errorf("unexpected argument: %s", arg)
 		}
 	}
 
-	if args.input == "" || args.banner == "" {
-		return args, fmt.Errorf("missing input or banner")
+	if args.input == "" {
+		return args, errors.New("input string is required")
 	}
 
 	return args, nil
@@ -71,11 +58,6 @@ func AsciiValue() string {
 	if err != nil {
 		PrintUsage()
 		return err.Error()
-	}
-
-	var alignment string
-	if len(args.flag) > 0 {
-		alignment = args.flag[0]
 	}
 
 	bannerPath := "banner/" + args.banner + ".txt"
@@ -89,12 +71,12 @@ func AsciiValue() string {
 		return err.Error()
 	}
 
-	printAsciiArt(args.input, bannerMap, alignment, termWidth)
+	printAsciiArt(args.input, bannerMap, args.flag, termWidth)
 	return ""
 }
 
 func PrintUsage() {
-	fmt.Println("Usage go run . [OPTION] [STRING] [BANNER]")
+	fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
 	fmt.Println()
-	fmt.Println("Example: go run . --align=right something standard")
+	fmt.Println("Example: go run . --align=right 'Hello World' standard")
 }
